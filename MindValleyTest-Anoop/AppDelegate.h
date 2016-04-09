7ee5ce4a56664e6a1edafbdc40e8d07c74a6e80e
Anoop Radhakrishnan <anoop@inkoniq.com>
2016-04-09 14:51:28 +0530

    Initial Commit

---
+++ MindValleyTest-Anoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MindValleyTest-Anoop
+//
+//  Created by Anoop Radhakrishnan on 09/04/16.
+//  Copyright © 2016 Anoop Radhakrishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MindValleyTest-Anoop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MindValleyTest-Anoop
+//
+//  Created by Anoop Radhakrishnan on 09/04/16.
+//  Copyright © 2016 Anoop Radhakrishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
